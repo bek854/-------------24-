37c90a20666268d8071826cae7380aa420fa1a01
bek854 <185514708+bek854@users.noreply.github.com>
2025-09-12 20:31:47 +0300

    Add all project files for Docker image

--- todo-list/cmd/main.go
+++ todo-list/cmd/main.go
@@ -1,4 +1,3 @@
-// cmd/main.go
 package main
 
 import (
@@ -15,13 +14,24 @@ func main() {
 
     r := chi.NewRouter()
 
+    // Добавляем корневой маршрут
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("TODO List API is working! Use /tasks endpoints"))
+    })
+
+    // Добавляем health check
+    r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("OK"))
+    })
+
+    // Регистрируем handlers для задач
     r.Get("/tasks", handlers.GetTasks)
     r.Post("/tasks", handlers.PostTask)
     r.Get("/tasks/{id}", handlers.GetTaskByID)
     r.Delete("/tasks/{id}", handlers.DeleteTask)
 
+    fmt.Println("Server starting on :3000")
     if err := http.ListenAndServe(":3000", r); err != nil {
         fmt.Printf("Start server error: %s", err.Error())
-        return
     }
 }
--- todo-list/database/database.go
+++ todo-list/database/database.go
@@ -19,20 +19,13 @@ type Dbinstance struct {
 var DB Dbinstance
 
 func ConnectDB() {
-	// создаём URL для соединения с базой данных.
-	// Имя пользователя базы данных, пароль и имя базы данных
-	// берутся из переменных окружения,
-	// они описаны в файле .env
 	dsn := fmt.Sprintf(
 		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Europe/Moscow",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
-	// создаём подключение к базе данных.
-	// В &gorm.Config настраивается логер,
-	// который будет сохранять информацию
-	// обо всех активностях с базой данных.
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
